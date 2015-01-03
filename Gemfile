b680b23779c4ba7d811047778786128fa82240e2
moutend <moutend@gmail.com>
2015-01-03 20:02:07 +0900

    Chore

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-gem "middleman", "~>3.3.7"
-gem "middleman-livereload", "~> 3.1.0"
-gem "slim"
--- config.rb
+++
@@ -1,7 +0,0 @@
-Time.zone = 'Tokyo'
-Slim::Engine.default_options[:pretty] = true
-Tilt::CoffeeScriptTemplate.default_bare = true
-
-set :css_dir,    'css'
-set :js_dir,     'js'
-set :images_dir, 'img'
